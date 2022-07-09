123320ec0964339318b72c93848f6fc7e4a63f57
anthonytruongg <anthonytruongg1@gmail.com>
2022-07-09 12:37:56 -0700

    Added form functionality

--- src/addTask.js
+++ src/addTask.js
@@ -1,10 +1,42 @@
-const addList = document.querySelector('.addList');
-addList.addEventListener('click', addTask);
+
+// const modal = document.querySelector('.modal');
+// const trigger = document.querySelector('.addTask');
+// const closeButton = document.querySelector('.close-button');
+
+// function toggleModal() {
+//     modal.classList.toggle('show-modal');
+// }
+
+// function windowOnClick(event) {
+//     if (event.target === modal) {
+//         toggleModal();
+//     }
+// }
+
+// trigger.addEventListener('click', toggleModal);
+// closeButton.addEventListener('click', toggleModal);
+// window.addEventListener('click', windowOnClick);
 
 
-function addTask(title, description, date, priority) {
-    this.title = title;
-    this.description = description;
-    this.date = date;
-    this.priority = priority;
+
+function createListForm() {
+    const modal = document.querySelector('.modal');
+    const trigger = document.querySelector('.addTask');
+    const closeButton = document.querySelector('.close-button');
+    
+    function toggleModal() {
+        modal.classList.toggle('show-modal');
+    }
+    
+    function windowOnClick(event) {
+        if (event.target === modal) {
+            toggleModal();
+        }
+    }
+    
+    trigger.addEventListener('click', toggleModal);
+    closeButton.addEventListener('click', toggleModal);
+    window.addEventListener('click', windowOnClick);
 }
+
+export default createListForm;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import createListForm from './addTask';
+
+
+createListForm();
+
